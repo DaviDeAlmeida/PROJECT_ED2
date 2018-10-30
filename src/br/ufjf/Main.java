e4a7e10e25d003a0a314bf991f1a147d21c83d58
Ruan Rodrigues <ruansjn@gmail.com>
2018-10-30 16:58:34 -0300

    Manutenção Main.java

--- src/br/ufjf/Main.java
+++ src/br/ufjf/Main.java
@@ -1,8 +1,7 @@
 package br.ufjf;
 
-import com.company.Hash.THash;
-
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main
 {
@@ -10,24 +9,18 @@ public class Main
 
     public static void main(String[] args) throws IOException
     {
-        //Leitura leitura = new Leitura(new Arquivo());
-        //ArrayList<Deputado> deputados = leitura.getDeputityInfos(1);
+        Leitura leitura = new Leitura(new Arquivo());
+        ArrayList<Deputado> deputados = leitura.getDeputityInfos();
 
-        //Integer teste1[] = Arrays.copyOfRange(leitura.getReceiptValues(), 3, 10);
-
-        THash hash = new THash();
-        hash.insert();
-        System.out.println("Indice:" + hash.getValueInHashVector(8));
-
-//        for(Deputado deputado : deputados){
-//            System.out.println("Nome: " + deputado.getDeputyName());
-//            for(Recibo recibo : deputado.getReceipt())
-//            {
-//                System.out.println(recibo.getEstablishmentName());
-//                System.out.println(recibo.getReceiptValue());
-//                System.out.println(recibo.getReceiptDescription());
-//            }
-//            System.out.println();
-//        }
+        for(Deputado deputado : deputados){
+            System.out.println("Nome: " + deputado.getDeputyName());
+            for(Recibo recibo : deputado.getReceipt())
+            {
+                System.out.println(recibo.getEstablishmentName());
+                System.out.println(recibo.getReceiptValue());
+                System.out.println(recibo.getReceiptDescription());
+            }
+            System.out.println();
+        }
     }
 }
